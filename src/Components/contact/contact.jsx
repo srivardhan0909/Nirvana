44f9108cdd8d1ce975ba863003ae3117388f8219
srivardhan <srivardhan@example.com>
2024-06-24 18:40:36 +0530

    new pp

--- src/Components/contact/contact.jsx
+++ src/Components/contact/contact.jsx
@@ -8,8 +8,8 @@ function Contact() {
         <h2>Connect with Us</h2>
         <p>I've noticed that you've been going through a tough time lately, and I care about your well-being. It's really important to take care of your mental health, just like your physical health. Talking to a therapist can provide you with the support and tools you need to navigate this challenging period.</p>
         <p className="tag-line">- Team Nirvana</p>
-        <p><strong>Email:</strong> <a href="mailto:jakkanisri0909@gmail.com">jakkanisri0909@gmail.com</a></p>
-        <p><strong>Phone:</strong> <a href="tel:+919177390913">9177390913</a></p>
+        <p><strong>Email:</strong> <a href="mailto:aditisk10@gmail.com">aditisk10@gmail.com</a></p>
+        <p><strong>Phone:</strong> <a href="tel:+919177390913">9930322456</a></p>
       </div>
     </div>
   );
