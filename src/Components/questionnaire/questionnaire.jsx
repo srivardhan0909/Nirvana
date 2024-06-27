d88907b01537bb2d3ef5787aafde8d7f484729f6
srivardhan <srivardhan@example.com>
2024-06-27 23:12:59 +0530

    new kk

--- src/Components/questionnaire/questionnaire.jsx
+++ src/Components/questionnaire/questionnaire.jsx
@@ -3,20 +3,53 @@ import { useNavigate } from 'react-router-dom';
 import './questionnaire.css';
 
 const preLoginQuestions = [
-  "How would you rate your overall mood, specifically in terms of feeling down, depressed, or hopeless?",
-  "How would you rate your level of interest or pleasure in activities you typically enjoy?",
-  "How would you rate your feelings of anxiety, worry, or being on edge?",
-  "How would you rate your sleep quality, in terms of experiencing difficulty sleeping or sleeping too much?",
+  {
+    question: "Rate your mood (down, depressed, or hopeless).",
+    imageUrl: "https://img.freepik.com/premium-photo/man-with-depression-mental-health-concept-ai-generated_145713-7751.jpg"
+  },
+  {
+    question: "Rate your interest or pleasure in activities.",
+    imageUrl: "https://img.freepik.com/premium-photo/vector-students-sitting-circle_731790-11862.jpg?w=360"
+  },
+  {
+    question: "Rate your anxiety or worry.",
+    imageUrl: "https://neurosciencenews.com/files/2021/11/ai-anxiety-neurosinces-public.jpg"
+  },
+  {
+    question: "Rate your sleep quality.",
+    imageUrl: "https://media.licdn.com/dms/image/D4D10AQGuFwgVhGNdqA/image-shrink_800/0/1690246803076?e=2147483647&v=beta&t=knzoIKCPnxU8IwWqz_Jv9jbNQ8EPjpbuFgs_4_aCZ-U"
+  }
 ];
 
 const postLoginQuestions = [
-  "How would you rate your overall mood today compared to when you first started using our website?",
-  "Have you noticed any improvements in your ability to manage stress since using our website? If so, please describe.",
-  "How often have you been practicing the coping strategies or exercises recommended by our website?",
-  "How effective do you find the resources and tools provided by our website in helping you manage your mental health?",
-  "How would you rate your memory and cognitive functions since using our website?",
-  "How happy do you feel overall compared to when you first started using our website?",
-  "How would you rate your ability to handle depressive symptoms since using our website?",
+  {
+    question: "Rate your mood today vs. when you started using our website.",
+    imageUrl: "https://via.placeholder.com/150?text=Mood+Today"
+  },
+  {
+    question: "Improvement in managing stress since using our website?",
+    imageUrl: "https://via.placeholder.com/150?text=Stress+Management"
+  },
+  {
+    question: "Frequency of practicing coping strategies from our website?",
+    imageUrl: "https://via.placeholder.com/150?text=Coping+Strategies"
+  },
+  {
+    question: "Effectiveness of our resources and tools for mental health?",
+    imageUrl: "https://via.placeholder.com/150?text=Mental+Health+Resources"
+  },
+  {
+    question: "Rate your memory and cognitive functions since using our website.",
+    imageUrl: "https://via.placeholder.com/150?text=Memory"
+  },
+  {
+    question: "Rate your overall happiness since using our website.",
+    imageUrl: "https://via.placeholder.com/150?text=Happiness"
+  },
+  {
+    question: "Rate your ability to handle depressive symptoms since using our website.",
+    imageUrl: "https://via.placeholder.com/150?text=Depressive+Symptoms"
+  }
 ];
 
 function Questionnaire() {
@@ -47,7 +80,7 @@ function Questionnaire() {
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
-      const poorResponses = responses.filter(response => response <= 3).length;
+      const poorResponses = responses.filter(response => response < 3).length;
       if (poorResponses > 2 && !isLoggedIn) {
         navigate('/contact');
       } else {
@@ -85,43 +118,65 @@ function Questionnaire() {
       <h1>Questionnaire</h1>
       <form onSubmit={handleSubmit}>
         <div className="question">
-          <label>{questions[currentQuestionIndex]}</label>
-          <div className="options">
-            <label>
-              <input
-                type="radio"
-                name="response"
-                value={1}
-                checked={responses[currentQuestionIndex] === 1}
-                onChange={() => handleResponseChange(1)}
-              />
-              <div>Poor</div>
-            </label>
-            {[2, 3, 4].map(value => (
-              <label key={value}>
+          <div className="content">
+            <label>{questions[currentQuestionIndex].question}</label>
+            <div className="options">
+              <label>
                 <input
                   type="radio"
-                name="response"
-                value={value}
-                checked={responses[currentQuestionIndex] === value}
-                onChange={() => handleResponseChange(value)}
-              />
+                  name="response"
+                  value={1}
+                  checked={responses[currentQuestionIndex] === 1}
+                  onChange={() => handleResponseChange(1)}
+                />
+                <div>Poor</div>
               </label>
-            ))}
-            <label>
-              <input
-                type="radio"
-                name="response"
-                value={5}
-                checked={responses[currentQuestionIndex] === 5}
-                onChange={() => handleResponseChange(5)}
-              />
-              <div>Good</div>
-            </label>
+              <label>
+                <input
+                  type="radio"
+                  name="response"
+                  value={2}
+                  checked={responses[currentQuestionIndex] === 2}
+                  onChange={() => handleResponseChange(2)}
+                />
+                <div>Unwell</div>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="response"
+                  value={3}
+                  checked={responses[currentQuestionIndex] === 3}
+                  onChange={() => handleResponseChange(3)}
+                />
+                <div>Average</div>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="response"
+                  value={4}
+                  checked={responses[currentQuestionIndex] === 4}
+                  onChange={() => handleResponseChange(4)}
+                />
+                <div>Well</div>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="response"
+                  value={5}
+                  checked={responses[currentQuestionIndex] === 5}
+                  onChange={() => handleResponseChange(5)}
+                />
+                <div>Good</div>
+              </label>
+            </div>
+            <div className="scale-bar">
+              <div className="scale-fill" style={{ width: `${getScalePercentage()}%` }}></div>
+            </div>
           </div>
-          <div className="scale-bar">
-            <div className="scale-fill" style={{ width: `${getScalePercentage()}%` }}></div>
-          </div>
+          <img src={questions[currentQuestionIndex].imageUrl} alt="question visual" />
         </div>
         <button type="submit">{currentQuestionIndex < questions.length - 1 ? 'Next' : 'Submit'}</button>
       </form>
